8cfca130909bfc2b0e467112594a421e721a9343
Pawel Marcinkiewicz <paweljmarcinkiewicz@gmail.com>
2021-04-30 22:16:42 +0100

    copied code into a new react project from the Codepen project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [quoteList, setQuoteList] = useState([]);
+  const [currentQuote, setCurrentQuote] = useState("");
+  const fetchQuotes = () => {
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setQuoteList(data);
+        setCurrentQuote(data[Math.floor(Math.random() * data.length)]);
+      });
+  };
+
+  const generateQuote = () => {
+    setCurrentQuote(quoteList[Math.floor(Math.random() * quoteList.length)]);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+  useEffect(() => {
+    generateQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row d-flex align-items-center justify-content-center">
+      <div id="quote-box" className="col-6 card mt-5">
+        <div className="card-body">
+          <h4 id="text" className="">
+            {currentQuote && currentQuote["text"]}
+          </h4>
+          <div className="row">
+            <p id="author" className="blockquote-footer col-8">
+              {currentQuote && currentQuote["author"]}
+            </p>
+            <a
+              id="new-quote"
+              onClick={generateQuote}
+              className="btn btn-light col-2"
+            >
+              New Quote
+            </a>
+            <a
+              href="twitter.com/intent/tweet"
+              target="_blank"
+              id="tweet-quote"
+              className="btn btn-light col-2"
+            >
+              Tweet
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
